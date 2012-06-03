ad45d895cde477ff7dc85128f4643b702d04edd9
Michael Wolfe <mswolfe@gmail.com>
2012-06-03 12:54:32 -0700

    Updating rabbit-common to use srfi/19 for date/string manipulations.  Adding a function to convert from time in a string format to time in milliseconds.  This function will now allow us to update the start/stop time of a task from the command line interface.

--- src/rabbit-common.rkt
+++ src/rabbit-common.rkt
@@ -1,8 +1,7 @@
 (module rabbit-common racket
   
   ; Requires
-  (require racket/date
-           racket/match)
+  (require (prefix-in time: srfi/19))
   
   ; Provides
   (provide (struct-out task)
@@ -12,6 +11,8 @@
            (struct-out total)
            
            time->date/string
+           date/string->time
+           
            time->hours/string)
   
   ; The task structure.
@@ -41,18 +42,52 @@
   ; Returns a string formatted date given the time in milliseconds.
   ;
   ; exact-integer? -> string?
-  (define (time->date/string ms [now (current-date)])
+  (define (time->date/string ms [now (time:current-date)])
     (if (= ms 0)
         "       "
-        (let* ((d (seconds->date (/ ms 1000)))
-               (str (date->string d #t)))
+        (let ((d (time:time-utc->date (time:make-time 'time-utc 0 (round (/ ms 1000))))))
           (if (and
-               (= (date-year d) (date-year now))
-               (= (date-year-day d) (date-year-day now)))
+               (= (time:date-year d) (time:date-year now))
+               (= (time:date-year-day d) (time:date-year-day now)))
             ; Printing a time for today.
-            (string-trim (substring str 11))
+            (time:date->string d "~H:~M:~S")
             ; Past day, print the whole thing.
-            str))))
+            (time:date->string d "~m/~d/~y ~H:~M:~S")))))
+  
+  ; Returns a date object representing the given date string.
+  ; Either time in ~H:~M:~S is accepted or time in 
+  ; ISO-8601 year-month-day-hour-minute-second format.
+  ;
+  ; string? -> (or date? boolean?)
+  (define (date/string->time str)
+    (let ((ms (with-handlers ([(lambda (e) (exn:fail? e))
+                     (lambda (e) #f)])
+                (let ((d (time:string->date str "~m/~d/~y ~H:~M:~S")))
+                  (if (time:date? d)
+                      (time->ms (time:date->time-utc d))
+                      #f)))))
+      (if (boolean? ms)
+          (with-handlers ([(lambda (e) (exn:fail? e))
+                           (lambda (e) #f)])
+            (let ((d (time:string->date (string-append (date-string) " " str) "~m/~d/~y ~H:~M:~S")))
+              (if (time:date? d)
+                  (time->ms (time:date->time-utc d))
+                  #f)))
+          ms)))
+  
+  ; Returns the current date in a string format including the month/day/year.
+  ;
+  ; void? -> string?
+  (define (date-string)
+    (time:date->string (time:current-date) "~m/~d/~y"))
+  
+  ; Returns the given time in milliseconds.
+  ;
+  ; time? -> exact-integer?
+  (define (time->ms t)
+    (+
+     (* (time:time-second t) 1000)
+     (/ (time:time-nanosecond t) 1000000)))
   
   ; Returns a string formatted date given the time in milliseconds.
   ;
@@ -62,4 +97,5 @@
       (string-append
         str
         " hrs")))
+  
   )
